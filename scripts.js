f32e3e78c0f89c106638ba40eff590c9be4dc1c3
silverfang890 <24977905+SilverFang890@users.noreply.github.com>
2020-11-02 09:46:09 -0800

    typo fix

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
 //Active Navbar
 var header = document.getElementById("nav");
-var tabs = header.getElementByClassName("tab");
+var tabs = header.getElementsByClassName("tab");
 for (var i = 0; i < tabs.length; i++) {
   console.log("i is " + i);
   tabs[i].addEventListener("click", function() {
-    var current = document.getElementByClassName("active");
+    var current = document.getElementsByClassName("active");
     if (current.length > 0) {
       current[0].className = current[0].className.replace("active", "");
     }
