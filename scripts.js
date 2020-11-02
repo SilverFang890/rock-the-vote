6f9869fa121d0dd1fa805d5571d3683bd19c7e1f
silverfang890 <24977905+SilverFang890@users.noreply.github.com>
2020-11-02 11:03:14 -0800

    going back to list approach

--- scripts.js
+++ scripts.js
@@ -1,13 +1,11 @@
 //Active Navbar
-var header = document.getElementById("nav");
-var tabs = header.getElementsByClassName("tab");
-for (var i = 0; i < tabs.length; i++) {
-  console.log("i is " + i);
-  tabs[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    if (current.length > 0) {
-      current[0].className = current[0].className.replace("active", "");
-    }
-    this.className += " active";
-  });
-}
+$(document).ready(function() {
+    $( ".navBar .tab" ).bind( "click", function(event) {
+        event.preventDefault();
+        var clickedItem = $( this );
+        $( ".navBar .tab" ).each( function() {
+            $( this ).removeClass( "active" );
+        });
+        clickedItem.addClass( "active" );
+    });
+});
