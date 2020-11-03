be46d908bea44179ad8386277a635ed536249f2d
silverfang890 <24977905+SilverFang890@users.noreply.github.com>
2020-11-02 17:37:53 -0800

    props

---
+++ scripts.js
@@ -0,0 +1,76 @@
+//descriptions for propositions
+const p14 = document.querySelector('.p14');
+const p15 = document.querySelector('.p15');
+const p16 = document.querySelector('.p16');
+const p17 = document.querySelector('.p17');
+const p18 = document.querySelector('.p18');
+const p19 = document.querySelector('.p19');
+const p20 = document.querySelector('.p20');
+const p21 = document.querySelector('.p21');
+const p22 = document.querySelector('.p22');
+const p23 = document.querySelector('.p23');
+const p24 = document.querySelector('.p24');
+const p25 = document.querySelector('.p25');
+
+//buttons for propositions
+const b14 = document.querySelector('.b14');
+const b15 = document.querySelector('.b15');
+const b16 = document.querySelector('.b16');
+const b17 = document.querySelector('.b17');
+const b18 = document.querySelector('.b18');
+const b19 = document.querySelector('.b19');
+const b20 = document.querySelector('.b20');
+const b21 = document.querySelector('.b21');
+const b22 = document.querySelector('.b22');
+const b23 = document.querySelector('.b23');
+const b24 = document.querySelector('.b24');
+const b25 = document.querySelector('.b25');
+
+//active toggle for propositions
+b14.addEventListener('click', ()=>{
+  p14.classList.toggle('active');
+});
+
+b15.addEventListener('click', ()=>{
+  p15.classList.toggle('active');
+});
+
+b16.addEventListener('click', ()=>{
+  p16.classList.toggle('active');
+});
+
+b17.addEventListener('click', ()=>{
+  p17.classList.toggle('active');
+});
+
+b18.addEventListener('click', ()=>{
+  p18.classList.toggle('active');
+});
+
+b19.addEventListener('click', ()=>{
+  p19.classList.toggle('active');
+});
+
+b20.addEventListener('click', ()=>{
+  p20.classList.toggle('active');
+});
+
+b21.addEventListener('click', ()=>{
+  p21.classList.toggle('active');
+});
+
+b22.addEventListener('click', ()=>{
+  p22.classList.toggle('active');
+});
+
+b23.addEventListener('click', ()=>{
+  p23.classList.toggle('active');
+});
+
+b24.addEventListener('click', ()=>{
+  p24.classList.toggle('active');
+});
+
+b25.addEventListener('click', ()=>{
+  p25.classList.toggle('active');
+});
